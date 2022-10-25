96a98bd680fade4feefff0b376ee16324e08f29c
Belisnalva Costa <bella-costa@hotmail.com.br>
2022-10-25 08:41:34 -0300

    Commit initial

---
+++ virtualGym/src/main/java/me/dio/virtualGym/VirtualGymApplication.java
@@ -0,0 +1,13 @@
+package me.dio.virtualGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualGymApplication.class, args);
+	}
+
+}
---
+++ virtualGym/src/test/java/me/dio/virtualGym/VirtualGymApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.virtualGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
